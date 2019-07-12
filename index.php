9f76a6948070f9328533eee8beef6af0f9eeca4e
Abdulkadir Kaya <37632855+akadirkayacom@users.noreply.github.com>
2019-07-12 22:34:56 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <title>Kur Scripti</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+
+<body>
+  <div class="header">
+    
+  </div>
+  
+  <div class="currency">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-2">
+        123
+        </div>
+        <div class="col-sm-2">
+        123
+        </div>
+        <div class="col-sm-2">
+        123
+        </div>
+        <div class="col-sm-2">
+        123
+        </div>
+      </div<
+    </div>
+  </div>
+</body>
